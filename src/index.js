2a3901601bec67f98d837cb7e93f5c21e3bc2dec
Unlqsting <noorbijapur500@gmail.com>
2023-05-18 12:32:54 -0700

    just for lucas :)

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
-import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
-
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
+import MainScene from "./MainScene.js";
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
     width: 800,
     height: 600,
-    scene: MyGame
-};
+    backgroundColor: '#333333',
+    type: Phaser.AUTO,
+    scene: [MainScene],
+    physics: {
+        default: 'arcade',
+        matter: {
+            debug: true,
+            gravity: {y: 0}
+        }
+    }
+    // plugins: {
+    //     scene: [
+    //         {
+    //             plugin: PhaserMatterCollisionPlugin,
+    //             key: 'matterCollision',
+    //             mapping: 'matterCollision'
+    //         }
+    //     ]
+    // }
+}
 
-const game = new Phaser.Game(config);
+new Phaser.Game(config)
