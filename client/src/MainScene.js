c4bb307c6c5362036ada4ba0f6d026e59d3361cd
SAnd353 <91165049+SAnd353@users.noreply.github.com>
2023-05-19 20:29:06 -0700

    addign stuff that seems to have been removed

--- client/src/MainScene.js
+++ client/src/MainScene.js
@@ -8,7 +8,7 @@ export default class MainScene extends Phaser.Scene {
 
     preload() {
 
-        this.load.spritesheet('player', player, { frameWidth: 32, frameHeight: 64 });
+        this.load.spritesheet('player', player, { frameWidth: 48, frameHeight: 48 });
         // this.load.setBaseURL('http://localhost:8080/');
     }
     create() {
@@ -19,9 +19,18 @@ export default class MainScene extends Phaser.Scene {
         	console.log('Connected!');
         });
 
-
         this.player = this.add.image(400,300, 'player')
         this.player.setScale(2);
+        //! player animation
+        // this.anims.create({
+        //     key: 'idle',
+        //     frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 2, 3, 4 ] }),
+        //     frameRate: 4,
+        //     repeat: -1
+        // });
+        // this.player.play('idle');
+
+        //! input keys
         this.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
@@ -29,21 +38,38 @@ export default class MainScene extends Phaser.Scene {
             right: Phaser.Input.Keyboard.KeyCodes.D
         });
         
+
+
+        // Add a platform as a rectangle
+        const platform = this.add.rectangle(400, 500, 200, 20, 0x00ff00);
+        
+        // Enable physics for the platform
+        this.physics.add.existing(platform, true);
+        
+        // Make the platform immovable
+        platform.body.immovable = true;
+
     }
     update() {
 
-        // console.log('update')
         const speed = 2.5;
         let playerVelocity = new Phaser.Math.Vector2();
         if (this.inputKeys.left.isDown) {
             playerVelocity.x = -1;
-        } else if (this.inputKeys.right.isDown) {
+            this.player.setFrame(7).setFlipX(true);
+
+        }
+        if (this.inputKeys.right.isDown) {
             playerVelocity.x = 1;
+            this.player.setFrame(7).setFlipX(false);
         } 
         if (this.inputKeys.up.isDown) {
             playerVelocity.y = -1;
-        } else if (this.inputKeys.down.isDown) {
+            this.player.setFrame(13);
+        }
+        if (this.inputKeys.down.isDown) {
             playerVelocity.y = 1;
+            this.player.setFrame(1);
         }
         playerVelocity.normalize().scale(speed);
         this.player.x += playerVelocity.x;
