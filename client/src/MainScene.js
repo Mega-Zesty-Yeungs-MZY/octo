ef6bf1d9e14a04c12b17fb0bf8e453ca105effec
SAnd353 <91165049+SAnd353@users.noreply.github.com>
2023-05-26 13:52:07 -0700

    Background fixes

--- client/src/MainScene.js
+++ client/src/MainScene.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-import background from './assets/jungle tileset.png';
+import background from './assets/background.png';
 import player from './assets/player.png';
 import obstacle from './assets/obstacle.png'
 import ObstacleClass from './classes/ObstaclesClass.js'
@@ -21,10 +21,11 @@ export default class MainScene extends Phaser.Scene {
     
     create() {        
         const self = this;
-        let bg = this.add.image(0,0, 'grass');
-        bg.setScale(5).setScrollFactor(1);
-        this.cameras.main.setBounds(0,0,bg.displayWidth/2, bg.displayHeight/2);
-        this.physics.world.setBounds(0, 0, bg.displayWidth/2, bg.displayHeight/2, true, true, true , true)
+        this.bg = this.add.image(0,0, 'grass');
+        this.bg.setScale(0.5).setScrollFactor(1);
+        this.bg.x = this.bg.displayWidth/2;
+        this.bg.y = this.bg.displayHeight/2;
+        this.physics.world.setBounds(0, 0, this.bg.displayWidth, this.bg.displayHeight, true, true, true , true)
         this.socket = io('http://localhost:3000', { transports: ['websocket'] });
 
         this.socket.on('connect', function () {
@@ -67,14 +68,13 @@ export default class MainScene extends Phaser.Scene {
         //! background        
 
        
-       // this.cameras.main.startFollow(this.player)
-
-
+        
+        
         this.obstacle = this.physics.add.sprite(400, 300, 'obstacle');    
         this.obstacle.setScale(0.1, 0.1);
-
         
-
+        
+        
         const staminaBarWidth = 200;
         const staminaBarHeight = 20;
         const staminaBarX = 400;
@@ -88,11 +88,11 @@ export default class MainScene extends Phaser.Scene {
         //! timer
         // Create the Timer instance
         this.timer = new Timer(this, 5000, this.timerCallback);
-
+        
         // Create and position the timer text
         const timerTextStyle = { font: '24px Arial', fill: '#ffffff' };
         this.timer.createTimerText(10, 10, timerTextStyle);
-
+        
         // Start the timer
         this.timer.start();
         
@@ -100,7 +100,7 @@ export default class MainScene extends Phaser.Scene {
     update() {
         if (this.player) { // Check if player object is defined before updating
             this.player.update();
-
+            
             var x = this.player.x;
             var y = this.player.y;
 
@@ -108,25 +108,25 @@ export default class MainScene extends Phaser.Scene {
                 console.log("emitted!");
                 this.socket.emit('heDothMoveth', {x : this.player.x, y : this.player.y});
             }
-
+            
             this.player.oldPosition = {
                 x : this.player.x,
                 y: this.player.y
             };
         }  
- 
+        
         this.timer.updateTimerText(); 
-
-
-
-    if (this.player && this.obstacle) {
-        if (this.physics.overlap(this.player, this.obstacle)) {
-            console.log("Collision detected!");
+        
+        
+        
+        if (this.player && this.obstacle) {
+            if (this.physics.overlap(this.player, this.obstacle)) {
+                console.log("Collision detected!");
+            }
         }
+        
+        // ...
     }
-
-    // ...
-}
     timerCallback() {
         // This function will be called when the timer duration is reached
         this.redLight = ! this.redLight;
@@ -135,12 +135,14 @@ export default class MainScene extends Phaser.Scene {
     }
     addPlayer(playerInfo){
         this.player = new PlayerClass(this, 300, 300, playerInfo.playerId);
+        this.cameras.main.startFollow(this.player)
+        this.cameras.main.setBounds(0, 0, this.bg.displayWidth, this.bg.displayHeight);
     }
     addOtherPlayers(playerInfo){
         const otherplayer = new PlayerClass(this, 300, 300);
-
+        
         otherplayer.playerId = playerInfo.playerId;
         this.otherPlayersGroup.add(otherplayer);
-
+        
     }
 }
